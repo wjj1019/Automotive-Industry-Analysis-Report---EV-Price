86d78970ab39332816eb494ed723ce6bb20960dc
wjj1019 <70126562+wjj1019@users.noreply.github.com>
2021-12-14 10:00:56 -0500

    Add files via upload

---
+++ Deployment/app.py
@@ -0,0 +1,31 @@
+from types import MethodDescriptorType
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+
+    float_features = [float(x) for x in request.form.values()]
+    final_features = np.array(float_features).reshape(1,-1)
+    prediction = model.predict(final_features)
+
+    return render_template('index.html', prediction_text = 'Price Should be  {}Euro'.format(prediction))
+
+@app.route('/results' , methods=['POST'])
+def results():
+    data = request.get_json(force=True) #Retrieve data as json
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug = True)
